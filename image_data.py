b03613e98b6c74d1d3cc6af1c900596141e6db4a
olfj <ole.fjeldsaa@live.com>
2025-09-08 13:22:30 +0200

    Updated dockstring, moved mthods from image.py into ImageData.

--- image_data.py
+++ image_data.py
@@ -1,14 +1,23 @@
-from image import load_images, images_to_grayscale_arr
 from exif import K_and_inv_from_exif
+from PIL import Image
+import glob
+import numpy as np
+import numpy.typing as npt
 
 class ImageData:
 	'''
 	A class to manage image data, including loading images, processing them into grayscale,
 	and extracting camera parameters from EXIF data.
 
-	Attributes:
-		__INITIAL_PAIR (dict): A private dictionary mapping dataset IDs to initial image pairs.
-		__PIXEL_TRESHOLD (dict): A private dictionary mapping dataset IDs to pixel thresholds.
+	### Attributes:
+	- images (list[Image]): a list of PIL images.
+	- paths (list[str]): a list of the paths to each image.
+	- K (np.ndarray): the calibration matrix of the camera used to take the images.
+	- K_inv (np.ndarray): the inverse of K.
+	- H, W (float, float): The height and width of the images.
+	- initial_pair (tuple[int, int]): the initial pair to initialize stereo vision.
+	- pixel treshold (int): the treshold for error when deciding what sift points are inliers and not. 
+
 	'''
 
 	__INITIAL_PAIR = {
@@ -26,21 +35,13 @@ class ImageData:
 		'''
 		Initializes the ImageData object for a specific dataset.
 
-		# Args:
-
+		### Args:
 		- dataset (int): The dataset ID to load and process.
-
-		# Attributes:
-		- images (list): A list of images loaded from the specified dataset path.
-		- grayscales (numpy.ndarray): A numpy array of grayscale images derived from the loaded images.
-		- K (numpy.ndarray): The camera intrinsic matrix extracted from the first image's EXIF data.
-		- K_inv (numpy.ndarray): The inverse of the camera intrinsic matrix.
-		- initial_pair (tuple): The initial image pair for the dataset.
-		- pixel_treshold (int): The pixel threshold for the dataset.
 		'''
+		
 		path = 'data/' + str(dataset) + '/'
-		self.images, self.paths = load_images(path)  # Load images from the dataset path
-		self.grayscales = images_to_grayscale_arr(self.images)  # Convert images to grayscale arrays
+		self.images, self.paths = self._load_images(path)  # Load images from the dataset path
+		self.grayscales = self._images_to_gray_arr()  # Convert images to grayscale arrays
 
 		# Extract camera intrinsic matrix and its inverse from EXIF data
 		self.K, self.K_inv = K_and_inv_from_exif(self.paths[0])
@@ -49,3 +50,46 @@ class ImageData:
 		# Retrieve initial pair and pixel threshold for the given dataset
 		self.initial_pair = self.__class__.__INITIAL_PAIR[dataset]
 		self.pixel_treshold = self.__class__.__PIXEL_TRESHOLD[dataset]
+
+	def _load_images(self, path
+			) -> tuple[list[Image.Image], list[str]]:
+		
+		'''
+		Loads every image in a folder containing (presumably) only images.
+
+		### Parameters:
+		- path (str): path to the image folder, should end with ''/''.
+		
+		### Returns
+		- images (list[Image.Image]): list of images loaded using PIL.
+		- paths (list[str]): list of paths to each specific image
+		'''
+
+		images = []
+		paths = []
+		
+		for f in glob.iglob(f'{path}*'):
+
+			images.append(Image.open(f))
+			paths.append(f)
+			
+
+		return images, paths
+	
+	def _images_to_gray_arr(self,) -> list[npt.NDArray]:
+		'''
+		Conwerts a PIL image to an array of grayscale values.
+		### Parameters:
+		- images (list[Image.Image]): a list of PIL images.
+		### Returns:
+		- grayscales (list[np.ndarray]): a list of the images in grayscale format, stored in ndarrays.
+		'''
+
+		grayscales = []
+
+		for img in self.images:
+
+			gray = img.convert('L')
+			grayscales.append(np.array(gray))
+
+		return grayscales
